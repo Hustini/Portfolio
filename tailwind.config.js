b9d3bb2456b2a6e764c722aa433d7ec32b1e6050
ejders-bzz <ejders@bzz.ch>
2024-05-07 12:43:03 +0200

    added new files to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html"
+    "./index.html",
+    "./html/me.html",
+    "./html/projects.html"
   ],
   theme: {
     extend: {},
