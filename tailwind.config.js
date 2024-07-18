274b4108587c2e7a7a7f9c9e043a9d3c655c46be
Hustini <ejdersalih02@gmail.com>
2024-07-18 11:52:57 +0200

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "index.html",
-    "./html/me.html",
-    "./html/projects.html"
+    "./index.html",
+    "./html/**/*.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
