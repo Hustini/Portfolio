202cf13be615a5b360ca42260c280c3a7c56dd28
Hustini <ejdersalih02@gmail.com>
2024-07-26 20:56:58 +0200

    had to change the javascript

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function Menu(e) {
+    console.log("debug thing");
     let list = document.querySelector("ul");
-    e.name === "menu" ? (e.name = "close", list.classList.add("top-[80px]"), list.classList.add("opacity-100")) :
-        (e.name = "menu", list.classList.remove("top-[80px]"), list.classList.remove("opacity-100"))
+    if (e.name === "menu") {
+        e.name = "close";
+        list.classList.add("top-[80px]");
+        list.classList.add("opacity-100");
+        list.classList.remove("top-[-400px]");
+        list.classList.remove("opacity-0");
+    } else {
+        e.name = "menu";
+        list.classList.remove("top-[80px]");
+        list.classList.remove("opacity-100");
+        list.classList.add("top-[-400px]");
+        list.classList.add("opacity-0");
+    }
 }
